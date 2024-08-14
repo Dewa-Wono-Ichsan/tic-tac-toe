348337726c51690b8a7b4c332b2a1c9552de8494
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-08-14 19:13:01 +0700

    Refacture input variable with type module pattern: IIFE
    Change method from push player symbol in tic tac toe field to splice method because push array method add element in last index of array while splice can add symbol player to different index of array.

--- script.js
+++ script.js
@@ -46,14 +46,15 @@ function match(){
 
 const input = (function(){
     const start = board().field
-    const input1 = () => start.splice(parseInt(prompt(`enter your position ${match().player1.name}`)),1,match().player1.symbol)
-    const input2 = () => start.splice(parseInt(prompt(`enter your position ${match().player2.name}`)),1,match().player2.symbol)
+    const input1 = () => start.splice(parseInt(prompt(`enter your position ${match().player1.name}, you get ${match().player1.symbol}`)), 1, match().player1.symbol)
+    const input2 = () => start.splice(parseInt(prompt(`enter your position ${match().player2.name}, you get ${match().player2.symbol}`)), 1 ,match().player2.symbol)
 
     return{start, input1, input2}
 })()
 
 
 function decide(){
+
     let chance = Math.random()
     if(chance <= 0.5){
         return 'player1'
@@ -63,11 +64,19 @@ function decide(){
 }
 
 function turn(player){
+    const board = input.start
+
     if(player === 'player1'){
+            
         input.input1()
-        return input.start
-    }else if(player === 'player2'){
         input.input2()
-        return input.start
+        return board
+    }else if(player === 'player2'){
+            
+        input.input2()
+        input.input1()
+        return board
     }
 }
+
+console.table(turn(decide()))
